9365069a0adbeb841dac7c43e59d375274974726
yatharth saxena <y.s.saxena7@gmail.com>
2020-06-03 03:35:02 +0530

    ERB COmplete

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,12 +28,30 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # config.middleware.use ActionDispatch::Flash
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.debug_exception_response_format = :default
+  
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'y.s.saxena7@gmail.com',
+  password:             'uknownothingjonsnow@77',
+  authentication:       :plain,
+  enable_starttls_auto: true
+}
+
+  config.active_job.queue_adapter = :sidekiq
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
